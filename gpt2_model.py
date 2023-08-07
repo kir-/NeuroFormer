dcf7cbd10172dc6cd4cff09834e376866f5bffc3
graysonl <graysonl@sfu.ca>
2023-08-07 00:13:44 -0700

    sent to device

--- gpt2_model.py
+++ gpt2_model.py
@@ -67,7 +67,8 @@ class GPT2EncoderLayer(nn.Module):
 
     def forward(self, src, src_mask=None, src_key_padding_mask=None):
         src = self.norm1(src)
-        src2 = self.self_attn(src, src, src, attn_mask=generate_square_subsequent_mask(len(src)), key_padding_mask=src_key_padding_mask)[0]
+        src_mask = generate_square_subsequent_mask(len(src)).to(src.device)
+        src2 = self.self_attn(src, src, src, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)[0]
         src = src + self.dropout1(src2)
         src = self.norm2(src)
         src2 = self.linear2(self.dropout(F.relu(self.layer1(src))))
